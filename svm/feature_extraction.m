1d36fe7d6ad4af8206fe4a048ac8b99e7691d637
Jinchao Ye <yjc111@gmail.com>
2013-03-05 20:24:40 -0800

    generate lexicons in matlab

--- svm/feature_extraction.m
+++ svm/feature_extraction.m
@@ -1,8 +1,20 @@
 %feature extraction
+clear all;
+close all;
+clc;
+
+
 fid = fopen('../review_polarity_dataset/lexicon.txt', 'r');
+values = [];
+N  = 2998;
+words = cell(N,1);
+count = 0;
 while ~feof(fid)
-    word = fscanf(fid, '%s', 1);
+    count = count + 1;
+    words{count} = fscanf(fid, '%s', 1);
     tmp = fscanf(fid, '%s', 1);
     value = fscanf(fid, '%f', 1);
+    values = [values; value];
     line = fgetl(fid);
 end
+save('lexicon_matlab', 'words', 'values');
